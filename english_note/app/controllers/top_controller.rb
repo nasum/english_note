ab2181d6010d163a4f1cb5593dad21aa6417c6cf
nasum <tomato.wonder.life@gmail.com>
2023-04-03 23:18:15 +0900

    fix login redirect

--- english_note/app/controllers/top_controller.rb
+++ english_note/app/controllers/top_controller.rb
@@ -1,4 +1,10 @@
 class TopController < ApplicationController
   skip_before_action :require_login, only: %i[index]
   layout 'top'
+
+  def index 
+    if current_user
+      redirect_to home_path
+    end
+  end
 end
