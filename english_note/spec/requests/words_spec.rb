fab1b2e4cb3ff1178dd269f6eda1f7d4b93e4864
nasum <tomato.wonder.life@gmail.com>
2023-04-04 00:32:39 +0900

    add word views

---
+++ english_note/spec/requests/words_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Words", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/words/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/words/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/words/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/words/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/words/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
