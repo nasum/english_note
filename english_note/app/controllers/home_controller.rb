0ab903b2d557521654b16568f5c1d3606a17f596
nasum <tomato.wonder.life@gmail.com>
2023-04-30 23:58:03 +0900

    add event section for home

--- english_note/app/controllers/home_controller.rb
+++ english_note/app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   # GET /home
-  def index; end
+  def index
+    @events = StudyEvent.where(user_id: current_user.id).order(created_at: :desc).limit(10)
+  end
 end
