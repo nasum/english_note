3a2d98cb2f7cf97e8b457e9be0ab14d16a56a660
nasum <tomato.wonder.life@gmail.com>
2023-05-01 00:55:27 +0900

    add recent words list for home

--- english_note/app/controllers/home_controller.rb
+++ english_note/app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   # GET /home
   def index
     @word = Word.new
+    @words = Word.where(user_id: current_user.id).order(created_at: :desc).limit(10)
     @events = StudyEvent.where(user_id: current_user.id).order(created_at: :desc).limit(10)
   end
 end
