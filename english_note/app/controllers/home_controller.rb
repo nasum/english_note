9f95968d3516d2cf078f8ee75b7f753cee668dd7
nasum <tomato.wonder.life@gmail.com>
2023-05-01 00:03:07 +0900

    add create word form for home

--- english_note/app/controllers/home_controller.rb
+++ english_note/app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   # GET /home
   def index
+    @word = Word.new
     @events = StudyEvent.where(user_id: current_user.id).order(created_at: :desc).limit(10)
   end
 end
